b43a60c743d73f902ab787dcf90d6d507d1027b5
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-05-31 14:49:27 -0500

    updated server for sequelize

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 
+const sequelize = require('./config/connection');
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(PORT, () => 
-    console.log(`Server running on port: ${PORT}`));
-    
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
